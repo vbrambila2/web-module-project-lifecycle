972935d58f714d346c81b39a27a437f0b527d82f
Victor Brambila <victorbrambila22@gmail.com>
2022-03-09 21:23:19 -0800

    tasks completed

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,109 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
+import Form from './Form';
+
+let idx = 0
+const getIdx = () => ++idx
 
 const URL = 'http://localhost:9000/api/todos'
 
+const initialState = {
+  successMessage: '',
+  list: [],
+  todo: {
+    todoInput: '',
+    completed: false
+  }
+}
+
 export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = initialState
+  }
+
+  componentDidMount() {
+    this.getTodos();
+  }
+
+  getTodos = () => {
+    axios.get(URL)
+      .then(res => {
+        this.setState({
+          ...this.state,
+          list: res.data.data,
+          successMessage: res.data.message
+        })
+      })
+      .catch(err => console.error(err))
+  }
+
+  addTodo = () => {
+    const newTodo = {
+      id: getIdx(),
+      name: this.state.todo.todoInput,
+      completed: false
+    }
+    axios.post(URL, newTodo)
+      .then(res => {
+        console.log(res.data.data, "data");
+        this.setState({
+          ...this.state,
+          list: [ ...this.state.list, res.data.data ]
+        })
+      })
+      .catch(err => console.error(err))
+  }
+
+  onChange = (key, value) => {
+    this.setState({
+      ...this.state,
+      todo: { ...this.state.todo, [key]: value },
+    })
+  }
+
+  setCompleted = id => {
+    axios.patch(`${URL}/${id}`)
+      .then(res => {
+        this.setState({
+          ...this.state,
+          list: this.state.list.map(li => {
+            return li.id === id ? { ...li, completed: res.data.data.completed } : li
+          })
+        })
+      })
+      .catch(err => console.error(err))
+  }
+
+  deleteTask = id => {
+    this.setState({
+      ...this.state, list: this.state.list.filter(item => item.id !== id)
+    })
+  }
+
   render() {
-    return null
+    const { list, todo } = this.state
+    console.log(list, "list");
+
+    return (
+      <div>
+        { this.state.successMessage }:
+        <ul>
+          {  
+            list.map(li => {
+              const { id, name, completed } = li
+              return (
+                <li key={id}>
+                  {name}
+                  <button onClick={() => this.setCompleted(id)}>Completed: { completed ? 'True' : 'False' }</button>
+                  <button onClick={() => this.deleteTask(id)}>Delete Task</button>
+                </li>
+              )
+            })
+          }
+        </ul>
+        <Form values={todo} onSubmit={this.addTodo} onChange={this.onChange} />
+      </div>
+    )
   }
 }
--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,7 +1,30 @@
 import React from 'react'
 
 export default class Form extends React.Component {
+  onSubmit = e => {
+    e.preventDefault();
+    this.props.onSubmit();
+  }
+
+  onChange = e => {
+    const { value, id } = e.target;
+    this.props.onChange(id, value);
+  }
+
   render() {
-    return null
+    const { values } = this.props;
+
+    return (
+      <form onSubmit={this.onSubmit}>
+        <input 
+          type="text"
+          id="todoInput"
+          placeholder="add todo"
+          onChange={this.onChange}
+          value={values.todoInput}
+        />
+        <input type="submit" />
+      </form>
+    )
   }
 }
